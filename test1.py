7afcfe5366b0859f475bcc68a16b73c6c67d80f8
Raymond Seger <raymondseger@live.com>
2017-09-02 12:33:11 +0700

    draw a rectangle

--- test1.py
+++ test1.py
@@ -5,6 +5,8 @@ img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
 
 cv2.line(img, (0,0), (150, 150), (255,255,255), 15)
 
+cv2.rectangle(img, (15,25), (200,150), (0,255,0), 5)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
