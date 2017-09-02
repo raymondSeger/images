859148b4afd444286d6ef455284ba468e396e24a
Raymond Seger <raymondseger@live.com>
2017-09-02 12:57:25 +0700

    ADAPTIVE_THRESH_GAUSSIAN_C

--- test1.py
+++ test1.py
@@ -9,9 +9,13 @@ retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
 
 retval2, threshold2 = cv2.threshold(grayscaled, 10, 255, cv2.THRESH_BINARY)
 
+th = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+
 cv2.imshow('original',img)
 cv2.imshow('threshold',threshold)
 cv2.imshow('threshold2',threshold2)
+
+cv2.imshow('Adaptive threshold',th)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
