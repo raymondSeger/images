95a23fb0cf3a52d8df73d75452ce79c43e051ca6
Raymond Seger <raymondseger@live.com>
2017-09-02 12:38:58 +0700

    add font

--- test1.py
+++ test1.py
@@ -18,6 +18,9 @@ pts = np.array([
 )
 cv2.polylines(img, [pts], True, (0, 255, 255), 3)
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV!', (0,130), font, 1, (200,255,255), 2, cv2.LINE_AA)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
