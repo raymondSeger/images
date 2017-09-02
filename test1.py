f7a1ec86acdcf55d2d6edf8f72e15e1f6c3196ed
Raymond Seger <raymondseger@live.com>
2017-09-02 12:45:19 +0700

    turn certain region to white and show it

--- test1.py
+++ test1.py
@@ -5,8 +5,10 @@ img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
 
 px = img[55,55] # [30 30 42] BGR = Blue Green Red)
 
-print(px)
-
 # ROI = Region of Image
-roi = img[100:150, 100:150]
-print(roi)
+img[100:150, 100:150] = [255, 255, 255]
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
