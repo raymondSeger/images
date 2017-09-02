305ec712b0b89817aa7188404cf60b3d074498e6
Raymond Seger <raymondseger@live.com>
2017-09-02 12:55:31 +0700

    threshold THRESH_BINARY
    A binary threshold is a simple "either or" threshold, where the pixels
    are either 255 or 0. In many cases, this would be white or black, but we
    have left our image colored for now, so it may be colored still. The
    first parameter here is the image. The next parameter is the threshold,
    we are choosing 10. The next is the maximum value, which we're choosing
    as 255. Next and finally we have the type of threshold, which we've
    chosen as THRESH_BINARY. Normally, a threshold of 10 would be somewhat
    poor of a choice. We are choosing 10, because this is a low-light
    picture, so we choose a low number. Normally something about 125-150
    would probably work best.

--- test1.py
+++ test1.py
@@ -1,18 +1,9 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
-
-px = img[55,55] # [30 30 42] BGR = Blue Green Red)
-
-# ROI = Region of Image
-img[100:150, 100:150] = [255, 255, 255]
-
-# copy some region BGR values and then paste that to another region BGR, effectively copy pasting it like photoshop
-watch_face = img[37:111, 107:194]
-img[0:74, 0:87] = watch_face
-
-cv2.imshow('image', img)
+img = cv2.imread('bookpage.jpg')
+retval, threshold = cv2.threshold(img, 12, 255, cv2.THRESH_BINARY)
+cv2.imshow('original',img)
+cv2.imshow('threshold',threshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
