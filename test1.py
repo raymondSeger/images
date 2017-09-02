70251b21890a51993b7576c9652d36a79b0cd3be
Raymond Seger <raymondseger@live.com>
2017-09-02 12:29:30 +0700

    output video file

--- test1.py
+++ test1.py
@@ -4,11 +4,14 @@ import numpy
 
 # start video capture
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID') #codec to use
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480)) #output and size and frame
 
 while True:
     # read it
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame) #write data to the output file
 
     # show it
     cv2.imshow('frame', frame)
@@ -19,4 +22,5 @@ while True:
         break
 
 cap.release()
+out.release() #release
 cv2.destroyAllWindows()
