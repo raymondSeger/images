0130052cd68ec12e5f6a060161574c657386ef7e
Raymond Seger <raymondseger@live.com>
2017-09-02 12:33:49 +0700

    draw a circle

--- test1.py
+++ test1.py
@@ -7,6 +7,8 @@ cv2.line(img, (0,0), (150, 150), (255,255,255), 15)
 
 cv2.rectangle(img, (15,25), (200,150), (0,255,0), 5)
 
+cv2.circle(img, (100, 63), 55, (0,0,255), -1)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
