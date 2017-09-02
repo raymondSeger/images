4cd81574af92944206013b434e73796d1757596d
Raymond Seger <raymondseger@live.com>
2017-09-02 12:16:25 +0700

    draw a line to an image

--- test1.py
+++ test1.py
@@ -1,10 +1,13 @@
-import cv2
+cv2=__import__('cv2')
 import matplotlib.pyplot as plt
 import numpy
 
 img = cv2.imread('watch.jpg', cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destryoAllWindows()
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#cv2.destryoAllWindows()
 
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.plot([50,100], [80,100], 'c', linewidth=5)
+plt.show()
