513e7e8434c30f27395445e478b9222970504370
Raymond Seger <raymondseger@live.com>
2017-09-02 12:26:30 +0700

    show gray video

--- test1.py
+++ test1.py
@@ -8,8 +8,15 @@ cap = cv2.VideoCapture(0)
 while True:
     # read it
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # show it
     cv2.imshow('frame', frame)
+
+    cv2.imshow('gray', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
