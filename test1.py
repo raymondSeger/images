5628f0d99ebfd4ef7a20503e507f694365373486
Raymond Seger <raymondseger@live.com>
2017-09-02 12:48:19 +0700

    # copy some region BGR values and then paste that to another region BGR, effectively copy pasting it like photoshop

--- test1.py
+++ test1.py
@@ -8,6 +8,10 @@ px = img[55,55] # [30 30 42] BGR = Blue Green Red)
 # ROI = Region of Image
 img[100:150, 100:150] = [255, 255, 255]
 
+# copy some region BGR values and then paste that to another region BGR, effectively copy pasting it like photoshop
+watch_face = img[37:111, 107:194]
+img[0:74, 0:87] = watch_face
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
