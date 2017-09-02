9166dbbc0e24b3ded8290443a6fe20071f126138
Raymond Seger <raymondseger@live.com>
2017-09-02 12:37:32 +0700

    add polygon

--- test1.py
+++ test1.py
@@ -9,6 +9,15 @@ cv2.rectangle(img, (15,25), (200,150), (0,255,0), 5)
 
 cv2.circle(img, (100, 63), 55, (0,0,255), -1)
 
+pts = np.array([
+    [10,5],
+    [20,30],
+    [70,20],
+    [50,10]],
+    np.int32
+)
+cv2.polylines(img, [pts], True, (0, 255, 255), 3)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
